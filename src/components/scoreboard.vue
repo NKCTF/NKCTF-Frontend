9e91f46bb03fd7846c8225d1641bc8ab7de9fc34
SunriseFox <sunrisefox@qq.com>
2018-10-03 15:33:10 +0800

    [Add] scoreboard

--- src/components/scoreboard.vue
+++ src/components/scoreboard.vue
@@ -3,11 +3,19 @@ import {mapState, mapMutations} from 'vuex';
 
 export default {
   name: 'scoreboard',
+  data() {
+    return {
+      scoreboard: [],
+    };
+  },
   beforeMount() {
 
   },
   mounted() {
-
+    APIFetch('/scoreboard/user').then(data => {
+      if(data.code === 0)
+        this.scoreboard = data.data;
+    })
   },
   destroyed() {
 
@@ -28,6 +36,21 @@ export default {
 
 <template>
 <div :id="$options.name" :class="$options.name">
-  7
+  <table class="table is-striped is-hoverable is-fullwidth">
+    <thead>
+    <tr>
+      <th>排名</th>
+      <th>用户名</th>
+      <th>分数</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="(user, idx) in scoreboard">
+      <td>{{idx}}</td>
+      <td>{{user.fields.username}}</td>
+      <td>{{user.fields.score}}</td>
+    </tr>
+    </tbody>
+  </table>
 </div>
 </template>
